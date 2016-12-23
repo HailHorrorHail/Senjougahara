d959ebaf787b7349cb1c15413cb8c391bcdd695e
Gerard Woods <gewoods@microsoff.com>
2016-12-22 22:41:01 -0800

    missed logging debug file

---
+++ DebuggingTool/Debugger/Debugger/DbLogger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Debugger
+{
+    class DbLogger
+    {
+        public static DbLogger Write = new DbLogger();
+
+
+        public void Information(string str, params object[] objs)
+        {
+            Console.WriteLine(str, objs);        
+        }
+
+        public void Error(string str, params object[] objs)
+        {
+            Console.WriteLine(str, objs);
+        }
+
+        public void Verbose(string str, params object[] objs)
+        {
+            //Console.WriteLine(str, objs);
+        }
+    }
+}
